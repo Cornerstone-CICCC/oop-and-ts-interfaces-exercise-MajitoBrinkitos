3dfe4ffe619e6d2e9dffefa2dab76b5733fd4325
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-10 09:56:41 -0800

    ex02.ts

--- dist/ex02.js
+++ dist/ex02.js
@@ -2,6 +2,8 @@
 // Exercise 2: Interfaces as Function Types
 // Create an interface for a function type that takes two numbers as arguments and returns their sum.
 // Implement the function using the interface and test it.
-const sum = (a, b) => a + b;
+const sum = (a, b) => {
+    return a + b;
+};
 console.log(sum(10, 20));
 // Expected Output: 30
