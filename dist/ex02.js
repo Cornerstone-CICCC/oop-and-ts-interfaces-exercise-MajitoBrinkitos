9bbb0ab56bda299126e37c5cc576b79d9a034a86
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-10 09:36:42 -0800

    ex01.ts

---
+++ dist/ex02.js
@@ -0,0 +1,7 @@
+"use strict";
+// Exercise 2: Interfaces as Function Types
+// Create an interface for a function type that takes two numbers as arguments and returns their sum.
+// Implement the function using the interface and test it.
+const sum = (a, b) => a + b;
+console.log(sum(10, 20));
+// Expected Output: 30
---
+++ dist/ex08.js
@@ -0,0 +1,14 @@
+"use strict";
+// Exercise 8: Optional Chaining
+// Create an interface for a "House" object with a nested "address" object that includes an optional "city" (string).
+// Use optional chaining to safely access the "city" and log it to the console.
+const myHouse = {
+    address: {
+        city: "New York"
+    }
+};
+console.log();
+// Expected Output: "New York"
+const noCityHouse = {};
+console.log();
+// Expected Output: "City not available"
