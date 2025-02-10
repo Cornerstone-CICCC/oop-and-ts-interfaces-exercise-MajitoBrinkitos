1d9a099737e33348ac188ed175d3288fd58a4412
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-10 10:57:55 -0800

    ex06.ts

--- src/ex06.ts
+++ src/ex06.ts
@@ -1,12 +1,13 @@
 // Exercise 6: Index Properties
 // Create an interface that defines a flexible dictionary where all properties are strings and their values are numbers.
 // Use the index signature to create an object and log the value of a dynamically added property.
-
 interface StringNumberDictionary {
-
+    [prop: string]: number;
 }
 
-const dictionary = {};
+const dictionary: StringNumberDictionary = {
+    
+};
 dictionary["one"] = 1;
 dictionary["two"] = 2;
 
